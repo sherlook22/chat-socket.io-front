0fef2168f60f257ab9bcffa0d112012f5ed53897
Gonzalo Re <gonzalore01@gmail.com>
2021-06-05 18:12:32 -0300

    feat: set the user to a room and save messages

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
+import './Chat.css'
 
 let socket
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
   const ENDPOINT = 'localhost:5000'
 
   useEffect(() => {
-    (() => {
       const { name, room } = queryString.parse(location.search)
 
       socket = io(ENDPOINT)
@@ -18,11 +20,39 @@ const Chat = ({ location }) => {
       setName(name)
       setRoom(room)
 
-      socket.emit('join', { name, room });
-    })()
+      socket.emit('join', { name, room }, () => {
+      })
+
+      return () => {
+        socket.emit('disconnect')
+        socket.off()
+      }
   }, [ENDPOINT, location.search])
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
   return (
-    <h1>Start Chating</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <input 
+          value={message} 
+          onChange={(event) => setMessage(event.target.value)} 
+          onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+        />
+      </div>
+    </div>
   )
 }
 
